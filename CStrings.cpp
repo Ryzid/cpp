a0cc0696ef9c3ed3c5de9fe1b23ee62784464362
Ryzid <aqwphishing@gmail.com>
2021-01-31 06:28:06 -0500

    Update and rename String Operators.cpp to CStrings.cpp

---
+++ CStrings.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <cstring>
+#include <cctype>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+//	double Weight {0};
+//	double MicroGram {0};
+//	double Dose {0};
+//	std::cout << "Gram to Tabs\nPurchased Product:";
+//	std::cin >> Weight;
+//	MicroGram = Weight*1000*1000; // mg*1000ug
+//	std::cout << "Dosage:";
+//	std::cin >> Dose;
+//	std::cout << "\n";
+//	std::cout << MicroGram/Dose << " Hits";
+
+//	char String1[20] {};
+//	int Str1l;
+//	char String2[20] {};
+//	int Str2l;
+//	char String3[50] {};
+//	char temp[50] {};
+//	
+//	std::cout << "Please enter First String\n";
+//	std::cin >> String1;
+//	Str1l = strlen(String1);
+//	std::cout << "Please enter Last String\n";
+//	std::cin >> String2;
+//	Str2l = strlen(String2);
+//	
+//	std::cout << String1 << " + " << String2 << " = ";
+//	
+//	strcpy(String3,String1);
+//	strcat(String3, " ");
+//	strcpy(String3, String2);
+//	
+//	std::cout << String3;
+	
+//	char Phrase[50];
+//	char temp[50];
+//	std::cout << "Enter Phrase:";
+//	std::cin.getline(Phrase, 16 /* to capture */);
+//	strcpy(temp,Phrase);
+//	if (strcmp(temp,Phrase) == 1){
+//		std::cout << "Error";
+//	};
+//	char first_name[10] {"Bjarne"};
+//    char last_name[10] {"Stroustrup"};
+
+
+
+	return 0;
+}
